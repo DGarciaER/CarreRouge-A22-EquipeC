a22c2ac38197cf9173798a23466086026f6adc79
Sadek Ahmed <e.asadek@etu.cvm.qc.ca>
2022-10-27 15:44:04 -0400

    menage du code et optimisation de la structure

--- ControleurJeu.py
+++ ControleurJeu.py
@@ -1,6 +1,6 @@
 from cgi import test
 from VueJeu import VueJeu
-from ModeleJeu import ModeleJeu
+from ModeleJeu import Bordure, CarreRouge, Rectangles
 from functools import partial
 import tkinter as tk
 
@@ -10,32 +10,28 @@ class ControleurJeu:
     
     def __init__(self, container):
         self.vueJeu = VueJeu()
-        self.modeleJeu = ModeleJeu(container)
-        self.modeleJeu.afficher_carreRouge()
-        self.modeleJeu.afficher_rectangleBleuSupDroit()
-        self.modeleJeu.afficher_rectangleBleuGauche()
-        self.modeleJeu.afficher_rectangleBleuInfDroit()
-        self.modeleJeu.afficher_rectangleBleuInfGauche()
-        self.modeleJeu.afficher_leftBorder()
-        self.modeleJeu.afficher_rightBorder()
-        self.modeleJeu.afficher_topBorder()
-        self.modeleJeu.afficher_bottomBorder()
+        self.carreRouge = CarreRouge(container)
+        self.rectangles = Rectangles(container)
+        self.bordure = Bordure(container)
+        self.vueJeu.afficherCarreRouge(self.carreRouge.carreRouge)
+        self.vueJeu.afficherRectanglesBlues(self.rectangles)
+        self.vueJeu.afficherBordure(self.bordure)
         self.enMouvement = False
         self.gameOver = False
         self.it = 0
         
-        self.modeleJeu.carreRouge.canvas.bind("<Button-1>", self.click)
-        self.modeleJeu.carreRouge.canvas.bind("<Motion>", self.move)
-        self.modeleJeu.carreRouge.canvas.bind("<ButtonRelease-1>", self.release)
+        self.carreRouge.carreRouge.canvas.bind("<Button-1>", self.click)
+        self.carreRouge.carreRouge.canvas.bind("<Motion>", self.move)
+        self.carreRouge.carreRouge.canvas.bind("<ButtonRelease-1>", self.release)
         
 
     # cette methode commence le jeu
     def click(self, e):
 
-        CRL = self.modeleJeu.carreRouge.get_position().x - 20    #position gauche du carré rouge 
-        CRR = self.modeleJeu.carreRouge.get_position().x + 20    #position droite du carré rouge
-        CRT = self.modeleJeu.carreRouge.get_position().y - 20    #position haut du carré rouge
-        CRB = self.modeleJeu.carreRouge.get_position().y + 20    #position bas du carré rouge
+        CRL = self.carreRouge.carreRouge.get_position().x - 20    #position gauche du carré rouge 
+        CRR = self.carreRouge.carreRouge.get_position().x + 20    #position droite du carré rouge
+        CRT = self.carreRouge.carreRouge.get_position().y - 20    #position haut du carré rouge
+        CRB = self.carreRouge.carreRouge.get_position().y + 20    #position bas du carré rouge
         
         if  e.x > CRL and e.x < CRR and e.y > CRT and e.y < CRB:
             self.enMouvement = True
@@ -51,53 +47,37 @@ class ControleurJeu:
         if self.enMouvement == True:
             if self.it % 3 == 0:    
                     
-                self.modeleJeu.carreRouge.translateTo(c31.Vecteur(e.x, e.y))
-                self.modeleJeu.carreRouge.set_position(c31.Vecteur(e.x,e.y))
-                self.modeleJeu.afficher_carreRouge()
-
-                collision = False
+                self.carreRouge.carreRouge.translateTo(c31.Vecteur(e.x, e.y))
+                self.carreRouge.carreRouge.set_position(c31.Vecteur(e.x,e.y))
+                self.vueJeu.afficherCarreRouge(self.carreRouge.carreRouge)
                 
-                for i in range(4):
-
-                    CRY = self.modeleJeu.carreRouge.get_position().y    #position Y milieu du carré rouge 
-                    CRX = self.modeleJeu.carreRouge.get_position().x    #position X milieu du carré rouge 
-                    CRL = CRX - 20    #position gauche du carré rouge 
-                    CRR = CRX + 20    #position droite du carré rouge
-                    CRT = CRY - 20    #position haut du carré rouge
-                    CRB = CRY + 20    #position bas du carré rouge
-                    PL = self.modeleJeu.listeRectangles[i].get_position().x - self.modeleJeu.listT[i][0]/2  #position gauche du pion
-                    PR = self.modeleJeu.listeRectangles[i].get_position().x + self.modeleJeu.listT[i][0]/2  #position droite du pion
-                    PT = self.modeleJeu.listeRectangles[i].get_position().y - self.modeleJeu.listT[i][1]/2  #position haut du pion
-                    PB = self.modeleJeu.listeRectangles[i].get_position().y + self.modeleJeu.listT[i][1]/2  #position bas du pion
-
-                    # la logique des collisions
-                    if  CRT <= PB and CRT >= PT or CRY <= PB and CRY >= PT:
-                        if  CRR >= PL and CRR <= PR:
-                            collision = True
-                            break
-                        elif CRL <= PR and CRL >= PL:
-                            collision = True
-                            break
-                        elif CRX <= PR and CRX >= PL:
-                            collision = True
-                            break
-
-                    elif  CRB >= PT and CRB <= PB or CRY>= PT and CRY <= PB:
-                        if  CRR >= PL and CRR <= PR:
-                            collision = True
-                            break
-                        elif CRL <= PR and CRL >= PL:
-                            collision = True
-                            break
-                        elif CRX <= PR and CRX >= PL:
-                            collision = True
-                            break                 
-                
-                if collision:
-                    print(True)
-                else:
-                    print(False)
-
+                self.collision()
             self.it += 1
+            
+    def collision(self):
+        collision = False
+            
+        for i in range(4):
 
-               
+            CRY = self.carreRouge.carreRouge.get_position().y    #position Y milieu du carré rouge 
+            CRX = self.carreRouge.carreRouge.get_position().x    #position X milieu du carré rouge 
+            CRL = CRX - 20    #position gauche du carré rouge 
+            CRR = CRX + 20    #position droite du carré rouge
+            CRT = CRY - 20    #position haut du carré rouge
+            CRB = CRY + 20    #position bas du carré rouge
+            PL = self.rectangles.listeRectangles[i].get_position().x - self.rectangles.listeTaillesRectangles[i][0]/2  #position gauche du pion
+            PR = self.rectangles.listeRectangles[i].get_position().x + self.rectangles.listeTaillesRectangles[i][0]/2  #position droite du pion
+            PT = self.rectangles.listeRectangles[i].get_position().y - self.rectangles.listeTaillesRectangles[i][1]/2  #position haut du pion
+            PB = self.rectangles.listeRectangles[i].get_position().y + self.rectangles.listeTaillesRectangles[i][1]/2  #position bas du pion
+
+            # la logique des collisions
+            if  CRT <= PB and CRT >= PT or CRY <= PB and CRY >= PT or CRB >= PT and CRB <= PB:
+                if  CRR >= PL and CRR <= PR or CRL <= PR and CRL >= PL or CRX <= PR and CRX >= PL:
+                    collision = True
+                    break
+        
+        if collision:
+            print(True)
+        else:
+            print(False)
+
--- ModeleJeu.py
+++ ModeleJeu.py
@@ -1,73 +1,36 @@
-from tkinter import Canvas
 import c31Geometry2 as c31
 
-
-class ModeleJeu:
-
+class CarreRouge:
     tailleCarreRouge = 40
-    coteAirDeJeu = 450
-
-    """Dans cette méthode (constructeur du ModeleJeu), on initialiser les éléments de l'aire de jeu."""
+    
     def __init__(self, container):
         
-        # initialisation du Carre carreRouge
+        # initialisation du Carre Rouge
         self.carreRouge = c31.Carre(container, c31.Vecteur(225,225),self.tailleCarreRouge,0, '#ff0000', '#ff0000', 0)
+
+
+class Rectangles:
+    listeTaillesRectangles = [[60, 60], [60, 50], [30, 60], [100, 20]]
+    
+    def __init__(self, container):
         
-        self.listT = [[60, 60], [60, 50], [30, 60], [100, 20]]
-        
+        # initialisation des Rectangles (Pions)
         self.listeRectangles = [
-                           c31.Rectangle(container, c31.Vecteur(100, 100), self.listT[0][0], self.listT[0][1], 0, '#0000ff', '#0000ff', 0),
-                           c31.Rectangle(container, c31.Vecteur(300, 85), self.listT[1][0], self.listT[1][1], 0, '#0000ff', '#0000ff', 0),
-                           c31.Rectangle(container, c31.Vecteur(85, 350), self.listT[2][0], self.listT[2][1], 0, '#0000ff', '#0000ff', 0),
-                           c31.Rectangle(container, c31.Vecteur(355, 340), self.listT[3][0], self.listT[3][1], 0, '#0000ff', '#0000ff', 0)
-                           ]
+                    c31.Rectangle(container, c31.Vecteur(100, 100), self.listeTaillesRectangles[0][0], self.listeTaillesRectangles[0][1], 0, '#0000ff', '#0000ff', 0),
+                    c31.Rectangle(container, c31.Vecteur(300, 85), self.listeTaillesRectangles[1][0], self.listeTaillesRectangles[1][1], 0, '#0000ff', '#0000ff', 0),
+                    c31.Rectangle(container, c31.Vecteur(85, 350), self.listeTaillesRectangles[2][0], self.listeTaillesRectangles[2][1], 0, '#0000ff', '#0000ff', 0),
+                    c31.Rectangle(container, c31.Vecteur(355, 340), self.listeTaillesRectangles[3][0], self.listeTaillesRectangles[3][1], 0, '#0000ff', '#0000ff', 0)
+                    ]
+
+class Bordure:
+    coteAirDeJeu = 450
+    
+    def __init__(self, container):
         
-        # initialisation du Rectangle leftBorder
-        self.leftBorder = c31.Rectangle(container, c31.Vecteur(25, self.coteAirDeJeu/2), 50, 450, 0,'black','black',0)
-
-        # initialisation du Rectangle rightBorder
-        self.rightBorder = c31.Rectangle(container, c31.Vecteur(425, self.coteAirDeJeu/2), 50, 450, 0,'black','black',0)          
- 
-        # initialisation du Rectangle topBorder
-        self.topBorder = c31.Rectangle(container, c31.Vecteur(self.coteAirDeJeu/2, 25), 450, 50, 0,'black','black',0)     
-
-        # initialisation du Rectangle bottomBorder
-        self.bottomBorder = c31.Rectangle(container, c31.Vecteur(self.coteAirDeJeu/2, 425), 450, 50, 0,'black','black',0)
-
-    """Cette méthode affiche le carré rouge."""
-    def afficher_carreRouge(self):
-        self.carreRouge.draw()
-        # cette fonctionnalité permet de déplacer carreRouge lorsque le boutton gauche de la souris est appuyé
-        # self.carreRouge.bind("<B1-Motion>", move)
-
-    """Cette méthode affiche le rectangle bleuGauche."""
-    def afficher_rectangleBleuGauche(self):
-        self.listeRectangles[0].draw()
-
-    """Cette méthode affiche le rectangle bleuSupDroit."""
-    def afficher_rectangleBleuSupDroit(self):
-        self.listeRectangles[1].draw()
-
-    """Cette méthode affiche le rectangle bleuInfGauche."""
-    def afficher_rectangleBleuInfGauche(self):
-        self.listeRectangles[2].draw()
-
-    """Cette méthode affiche le rectangle bleuInfDroit."""
-    def afficher_rectangleBleuInfDroit(self):
-        self.listeRectangles[3].draw()
-
-    """Cette méthode affiche le rectangle leftBorder."""
-    def afficher_leftBorder(self):
-        self.leftBorder.draw()
-
-    """Cette méthode affiche le rectangle rightBorder."""
-    def afficher_rightBorder(self):
-        self.rightBorder.draw()
-
-    """Cette méthode affiche le rectangle topBorder."""
-    def afficher_topBorder(self):
-        self.topBorder.draw()
-
-    """Cette méthode affiche le rectangle bottomBorder."""
-    def afficher_bottomBorder(self):
-        self.bottomBorder.draw()    
+        # initialisation de la Bordure
+        self.listeBordures = [
+                    c31.Rectangle(container, c31.Vecteur(25, self.coteAirDeJeu/2), 50, 450, 0,'black','black',0),
+                    c31.Rectangle(container, c31.Vecteur(425, self.coteAirDeJeu/2), 50, 450, 0,'black','black',0),
+                    c31.Rectangle(container, c31.Vecteur(self.coteAirDeJeu/2, 25), 450, 50, 0,'black','black',0),
+                    c31.Rectangle(container, c31.Vecteur(self.coteAirDeJeu/2, 425), 450, 50, 0,'black','black',0)
+        ]
